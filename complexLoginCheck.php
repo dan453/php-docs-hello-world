9adbe0e43c282dc951e8e10581b7fdcba178b87a
dan453 <75531319+dan453@users.noreply.github.com>
2023-12-28 16:14:33 +0000

    Create complexLoginCheck.php

---
+++ complexLoginCheck.php
@@ -0,0 +1,9 @@
+<?php
+  echo "<form action='complexLoginCheck.php' method='POST'>";
+  echo "Username ";
+  echo "<input name='txtUsername' type='text' />";
+  echo "<br />Password ";
+  echo "<input name='txtPassword' type='password' />";
+  echo "<br /><input type='submit' value='Login'";
+  echo "</form>";
+?>
